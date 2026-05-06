a6d410339a366a38866dd2e99fdc9dfa084ada3a
sohel <ameersohel42@gmail.com>
2026-05-06 12:02:02 +0530

    reupdating the base

--- src/test/java/pageObjects/BasePage.java
+++ src/test/java/pageObjects/BasePage.java
@@ -39,7 +39,6 @@ public class BasePage
 	
 	public WebElement waitForVisibility(WebElement element)
 	{
-		System.out.println("This is conflict");
 		return wait.until(ExpectedConditions.visibilityOf(element));
 	}
 	
